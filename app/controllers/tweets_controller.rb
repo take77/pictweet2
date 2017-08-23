cbe212797ec272d25cdfa77088090d65190037c3
masakazu takeda <teke77@toki.waseda.jp>
2017-08-23 19:42:51 +0900

    pagination

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.order("id DESC")
+    @tweets = Tweet.order("id DESC").page(params[:page]).per(5)
   end
 
   def new
