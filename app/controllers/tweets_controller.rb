618db3774617e812ee4528f1719590af0e27fa1a
masakazu takeda <teke77@toki.waseda.jp>
2017-08-25 18:54:36 +0900

    curriculums/527 finish

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,19 +3,37 @@ class TweetsController < ApplicationController
 before_action :move_to_index, except: :index
 
   def index
-    @tweets = Tweet.order("id DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
   end
 
   def move_to_index
